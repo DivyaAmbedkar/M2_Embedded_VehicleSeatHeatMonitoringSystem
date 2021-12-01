8b01553c46eaaa15fe31d35e270fbaa86ba69f19
DivyaAmbedkar <94296796+DivyaAmbedkar@users.noreply.github.com>
2021-12-01 21:32:11 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,3 +1,16 @@
+/**
+ * @file main.c
+ * @author Divya
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+
 #include <avr/io.h>
 #include"Heat1.h"
 #include"Heat2.h"
