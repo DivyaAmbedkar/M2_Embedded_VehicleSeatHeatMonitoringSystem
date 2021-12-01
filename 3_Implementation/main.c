3372ab5fb70fcfc1905288f6a73784071d2c59ec
DivyaAmbedkar <94296796+DivyaAmbedkar@users.noreply.github.com>
2021-12-01 13:30:09 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,16 +1,16 @@
 #include <avr/io.h>
-#include"A1.h"
-#include"A2.h"
-#include"A3.h"
+#include"Heat1.h"
+#include"Heat2.h"
+#include"Heat3.h"
 #include<util/delay.h>
 int main(void)
 {
     LCD_INITIALIZATION();
     while(1)
     {
-        A1(); //Activity 1
-        A2(); //Activity 2
-        A3(); //Activity 3
+        Heat1(); //Activity 1
+        Heat2(); //Activity 2
+        Heat3(); //Activity 3
     }
   return 0;
 }
